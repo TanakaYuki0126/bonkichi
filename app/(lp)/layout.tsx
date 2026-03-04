febfcbda39412806ce5e1a3871a1ce55b43ebc9a
Tanaka Yuki <tnkyk0126@gmail.com>
2026-03-04 21:35:01 +0900

    layoutファイルを修正

--- app/(lp)/layout.tsx
+++ app/(lp)/layout.tsx
@@ -1,44 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 import ThreeCanvas from "@/components/three/ThreeCanvas";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "bonkichi",
   description: "bonkichi",
 };
 
-export default function RootLayout({
+export default function LpLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    <div>
+      <div
+        id="introOverlay"
+        className="flex justify-center fixed inset-0 bg-black z-50 pointer-events-none"
       >
-        <div
-          id="introOverlay"
-          className="flex justify-center fixed inset-0 bg-black z-50 pointer-events-none"
-        >
-          <p className="text-white text-center fixed top-1/2 text-xl tracking-[0.3em]">
-            small cabin, big journey
-          </p>
-        </div>
-        <ThreeCanvas />
-        {children}
-      </body>
-    </html>
+        <p className="text-white text-center fixed top-1/2 text-xl tracking-[0.3em]">
+          small cabin, big journey
+        </p>
+      </div>
+      <ThreeCanvas />
+      {children}
+    </div>
   );
 }
--- app/diary/layout.tsx
+++ app/diary/layout.tsx
@@ -1,38 +1,13 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
 import LandingMenu from "@/components/three/LandingMenu";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "bonkichi",
-  description: "bonkichi",
-};
-
-export default function RootLayout({
+export default function DiaryLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <main className="relative bg-lime-900 h-screen">
-          <LandingMenu />
-          {children}
-        </main>
-      </body>
-    </html>
+    <div className="relative bg-lime-900 h-screen">
+      <LandingMenu />
+      {children}
+    </div>
   );
 }
