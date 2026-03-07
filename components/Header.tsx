c700542ec2cc75655f35d50848c1becabbb61926
Tanaka Yuki <tnkyk0126@gmail.com>
2026-03-07 16:30:57 +0900

    ハンバーガーメニュー実装

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 
 function UnderlineLink({
   href,
@@ -20,19 +21,55 @@ function UnderlineLink({
 }
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
   return (
-    <header className={`fixed top-0 left-0 w-full z-50`}>
-      <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between text-gray-900">
+    <header
+      className={`fixed top-0 left-0 w-full z-50 ${
+        open ? "bg-white opacity-90" : ""
+      }`}
+    >
+      <div className="max-w-6xl mx-auto px-6 h-16 flex items-center justify-between text-gray-700">
         <Link href="/" className="text-lg hover:opacity-70 transition">
-          ぼんきち - small cabin, big journey -
+          ぼんきち
         </Link>
-        <nav className="flex items-center gap-6 text-sm">
+        <nav className="hidden items-center gap-6 text-sm md:flex">
           <UnderlineLink href="/about">about / 紹介</UnderlineLink>
-          <UnderlineLink href="/diary">diary / 日誌</UnderlineLink>
           <UnderlineLink href="/gallery">gallery / ギャラリー</UnderlineLink>
+          <UnderlineLink href="/diary">diary / 日記</UnderlineLink>
           <UnderlineLink href="/gallery">contact / 連絡</UnderlineLink>
+          <UnderlineLink href="/login">login / ログイン</UnderlineLink>
         </nav>
+        <button className="md:hidden" onClick={() => setOpen((prev) => !prev)}>
+          <div className="relative w-8 h-8">
+            <span
+              className={`absolute bg-gray-700 left-0 top-1/2 h-0.5 w-8 transition-all duration-300 ${
+                open ? "rotate-45 translate-y-0" : "-translate-y-2"
+              }`}
+            ></span>
+            <span
+              className={`absolute bg-gray-700 left-0 top-1/2 h-0.5 w-8 transition-all duration-300 ${
+                open ? "opacity-0" : ""
+              }`}
+            ></span>
+            <span
+              className={`absolute bg-gray-700 left-0 top-1/2 h-0.5 w-8 transition-all duration-300 ${
+                open ? "-rotate-45 translate-y-0" : "translate-y-2"
+              }`}
+            ></span>
+          </div>
+        </button>
       </div>
+      <nav
+        className={`px-6 flex flex-col items-center gap-3 md:hidden  bg-white h-screen  transition-opacity duration-300 ${
+          open ? "opacity-90" : "opacity-0 pointer-events-none"
+        }`}
+      >
+        <UnderlineLink href="/about">about / 紹介</UnderlineLink>
+        <UnderlineLink href="/gallery">gallery / ギャラリー</UnderlineLink>
+        <UnderlineLink href="/diary">diary / 日記</UnderlineLink>
+        <UnderlineLink href="/gallery">contact / 連絡</UnderlineLink>
+        <UnderlineLink href="/login">login / ログイン</UnderlineLink>
+      </nav>
     </header>
   );
 }
