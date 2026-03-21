5a6507090802af859bb9d0b202e8d5a6b34f7dd7
Tanaka Yuki <tnkyk0126@gmail.com>
2026-03-21 21:30:36 +0900

    モバイル画像のvh指定を辞める

--- app/about/ParallaxImageMobile.tsx
+++ app/about/ParallaxImageMobile.tsx
@@ -33,7 +33,7 @@ export default function ParallaxImageMobile({
   return (
     <div
       ref={ref}
-      className="shrink-0 w-screen h-[50dvh] relative overflow-hidden"
+      className="shrink-0 w-screen h-[500px] relative overflow-hidden"
     >
       <Image
         src={src}
