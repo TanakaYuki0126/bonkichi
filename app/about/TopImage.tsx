2665211c4aa8c1413f8bce1b5bc8940c3f0d32ec
Tanaka Yuki <tnkyk0126@gmail.com>
2026-03-23 13:45:03 +0900

    トップ画像のパララックスが効いてなかったのを修正

--- app/about/TopImage.tsx
+++ app/about/TopImage.tsx
@@ -2,21 +2,27 @@
 
 import { useScroll } from "@/contexts/ScrollContext";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 export default function TopImage() {
-  const { smoothedProgress } = useScroll();
-  const translateX = -smoothedProgress * 500;
+  const ref = useRef<HTMLImageElement>(null);
+  const { subscribe } = useScroll();
+  useEffect(() => {
+    return subscribe(({ smoothedScrollY }) => {
+      ref.current?.style.setProperty("--p", smoothedScrollY.toString());
+    });
+  }, [subscribe]);
   return (
-    <div className="flex items-end absolute bottom-10 right-10 xl:right-20">
+    <div
+      ref={ref}
+      className="flex items-end absolute bottom-10 right-10 xl:right-20 top-image-parallax"
+    >
       <Image
         src="/about/top_image.jpg"
         height={3627}
         width={5455}
         alt="top_image"
-        className="w-[40vw] lg:w-[50vw] rounded-full animate-circleIn transition-transform duration-100"
-        style={{
-          transform: `translate3d(${translateX}px,0,0)`,
-        }}
+        className="w-[40vw] lg:w-[50vw] rounded-full duration-100 animate-circleIn"
       />
     </div>
   );
