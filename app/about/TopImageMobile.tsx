1bcb18a42adcc1fad1b2604fa1af9f047fcd2a68
Tanaka Yuki <tnkyk0126@gmail.com>
2026-03-23 13:00:09 +0900

    TOP画像にintersectionObserver追加

--- app/about/TopImageMobile.tsx
+++ app/about/TopImageMobile.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
 import Image from "next/image";
+import { useRef } from "react";
 
 export default function TopImageMobile() {
+  const ref = useRef<HTMLImageElement>(null);
+  useIntersectionObserver(ref, () =>
+    ref.current?.classList.add("animate-fadeIn")
+  );
   return (
     <Image
+      ref={ref}
       src="/about/top_image.jpg"
       height={3627}
       width={5455}
       alt="top_image"
       className="animate-fadeIn"
-      style={{ animationDelay: "3s" }}
+      style={{ animationDelay: "1s" }}
     />
   );
 }
