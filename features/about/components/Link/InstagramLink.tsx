500292f271b65276564bdfd311218b3d6c77b7f5
Tanaka Yuki <tnkyk0126@gmail.com>
2026-04-30 10:45:30 +0900

    インスタのリンクを更新

--- features/about/components/Link/InstagramLink.tsx
+++ features/about/components/Link/InstagramLink.tsx
@@ -2,7 +2,7 @@ import { FaInstagram } from "react-icons/fa";
 export default function InstagramLink() {
   return (
     <a
-      href="https://www.instagram.com/bonkichi.traveller"
+      href="https://www.instagram.com/smallcabin.slowjourney"
       target="_blank"
       rel="noopener noreferrer nofollow"
     >
